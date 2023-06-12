26761b94936b8b34ffc5648ec1efb28a2c62e2cb
Ebrahim Shamskilani <e.shams@tci.ir>
2023-06-12 21:11:24 +0200

    create organ clases

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 
+// Base Organ class
+class Organ {
+    constructor(type, price) {
+      this.type = type;
+      this.price = price;
+    }
+  
+    purchase(cash) {
+      return Math.floor(cash / this.price);
+    }
+  }
+  
+  // Organ subclasses
+  class Heart extends Organ {
+    constructor(price) {
+      super('Heart', price);
+    }
+  }
+  
+  class Liver extends Organ {
+    constructor(price) {
+      super('Liver', price);
+    }
+  }
+  
+  class Lung extends Organ {
+    constructor(price) {
+      super('Lung', price);
+    }
+  }
+  
+
 // Function to process the orders from a CSV file
 function processOrdersFromFile(filePath) {
     const orders = [];
